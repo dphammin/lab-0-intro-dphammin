b67015d9f73ae5df54a79839b2401334bcb06446
dphammin <144369295+dphammin@users.noreply.github.com>
2025-01-16 19:25:19 -0500

    finished step 3

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void) {
+    int x = 0;  
+    int y = 0;  
+
+    while (1) { 
+        x++;
+        if (x >= 10) { 
+            x = 0; 
+            y++;   
+        }
+    }
+    return 0; 
+}
